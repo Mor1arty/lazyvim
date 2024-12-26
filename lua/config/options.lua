93a924438b6df3ff303bf45f2c2454ea1d043d6b
Moriarty <shengyanggao98@gmail.com>
2024-12-26 09:18:46 +0800

    Backup lazyvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 
 vim.opt.winbar = "%=%m %f %r%h%w%y"
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
